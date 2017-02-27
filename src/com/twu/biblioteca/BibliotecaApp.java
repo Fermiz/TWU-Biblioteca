c067d40d0037f9c3f88f3f533eebc11df88d9d7c
lishuangqi <hilishuangqi@hotmail.com>
2017-02-27 10:13:54 +0800

    first commit

---
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Bangalore Public Library!");
+    }
+
+    //Welcome Message
+    //List Books
+    //Book Details
+    //Main Menu
+    //Invalid Menu Option
+    //Quit
+    //Checkout Book
+    //Successful Checkout
+    //Unsuccessful Checkout
+    //Return Book
+    //Successful Return
+    //Unsuccessful Return
+}
