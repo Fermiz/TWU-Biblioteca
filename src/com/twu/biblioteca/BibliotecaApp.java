c486a49dcaef948263607c5c90b14537b7ecd026
lishuangqi <hilishuangqi@hotmail.com>
2017-02-27 13:45:56 +0800

    adjust Scanner class to InputStream when jUnit test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,17 +1,21 @@
 package com.twu.biblioteca;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BibliotecaApp {
 
     private ArrayList<Book> bookList;
+    private final PrintStream out;
     private Scanner scanner;
     private Boolean inputValid;
 
-    public BibliotecaApp(ArrayList<Book> listOfBooks,Scanner sc) {
+    public BibliotecaApp(ArrayList<Book> listOfBooks, InputStream in, PrintStream out) {
         this.bookList = listOfBooks;
-        this.scanner =sc;
+        this.out = out;
+        this.scanner = new Scanner(in);
         this.inputValid = true;
     }
 
@@ -24,14 +28,14 @@ public class BibliotecaApp {
                 this.listBooks(this.bookList);
                 break;
             default:
-                System.out.println("Select a valid option!");
+                this.out.println("Select a valid option!");
         }
     }
 
     private Integer getUserInput() {
         Integer input = 0;
         try {
-            System.out.print("\nMenu selection: \n");
+            this.out.print("\nMenu selection: \n");
             input = this.scanner.nextInt();
         } catch (Exception e) {
 
@@ -50,19 +54,19 @@ public class BibliotecaApp {
     }
 
     public void quit(){
-        System.out.println("See you next time!");
+        this.out.println("See you next time!");
         this.inputValid = false;
     }
 
     public void getWelcomeMessage(){
-        System.out.println("Welcome To The Biblioteca Library System!");
+        this.out.println("Welcome To The Biblioteca Library System!");
         this.showMenuOptions();
     }
 
     public void showMenuOptions() {
-        System.out.println("Menu Options:");
-        System.out.println("1.Enter [1] to show all books");
-        System.out.println("2.Enter [0] to show quit the system");
+        this.out.println("Menu Options:");
+        this.out.println("1.Enter [1] to show all books");
+        this.out.println("2.Enter [0] to show quit the system");
         this.respondToUserInput();
     }
 
@@ -71,7 +75,7 @@ public class BibliotecaApp {
         for (Book book : bookList){
             output += book.getBookInfo() + "\n";
         }
-        System.out.print(output);
+        this.out.print(output);
     }
 
 
@@ -83,7 +87,7 @@ public class BibliotecaApp {
         bookList.add(new Book("Life of Pi", "Yann Martel", 2007));
         bookList.add(new Book("A Song of Ice and Fire", "George R.R. Martin", 2012));
 
-        BibliotecaApp biblioteca = new BibliotecaApp(bookList, new Scanner(System.in));
+        BibliotecaApp biblioteca = new BibliotecaApp(bookList, System.in, new PrintStream(System.out));
 
         biblioteca.start();
     }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,13 +3,15 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static com.sun.javaws.JnlpxArgs.verify;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by Mac on 2017/2/27.
@@ -18,20 +20,21 @@ public class BibliotecaAppTest {
     private BibliotecaApp bibliotecaApp;
     private ArrayList<Book> bookList;
     private Book theBookThief;
-    private Scanner scanner;
+    private InputStream in;
+    private PrintStream out;
 
     @Before
     public void setUp(){
         bookList = new ArrayList();
-        scanner = mock(Scanner.class);
-        bibliotecaApp = new BibliotecaApp(bookList,scanner);
+        in = mock(InputStream.class);
+        out = mock(PrintStream.class);
         theBookThief = mock(Book.class);
-
+        bibliotecaApp = new BibliotecaApp(bookList,in,out);
     }
 
     @Test
-    public void shouldPrintWelcomeMessageWhenStarting(){
+    public void shouldPrintWelcomeMessageWhenStart(){
         bibliotecaApp.start();
-        //verify(System.out).println(contains("Welcome"));
+        verify(out).println(contains("Welcome"));
     }
 }
